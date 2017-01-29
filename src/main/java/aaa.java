3a52c8398ec71b9d79ce0b9e9b033f4cbc5617b4
ipdeveloper <pourreza@gmail.com>
2017-01-29 16:05:28 +0330

    aaa

---
+++ src/main/java/aaa.java
@@ -0,0 +1,24 @@
+//http://dunro.com/api/v1.3/business/show/584622aa8efaa
+//select *
+//        from advers s pivot(max(value) for key in('lng',
+//        'slugged_title',
+//        'rates',
+//        'uuid',
+//        'guilds',
+//        'status',
+//        'has_active_contract',
+//        'id',
+//        'title',
+//        'slogan',
+//        'logo',
+//        'knots',
+//        'tel_1',
+//        'description',
+//        'lat',
+//        'address'
+//
+//        ))
+
+
+
+
